eea4c91f36e001ab57dd1b30ff0b4cfa021585c6
Jang <hjjang501@gmail.com>
2025-05-13 13:04:42 +0900

    Add solution code 5/13/25

---
+++ programmers/131537.sql
@@ -0,0 +1,33 @@
+/*
+프로그래머스
+오프라인/온라인 판매 데이터 통합하기
+https://school.programmers.co.kr/learn/courses/30/lessons/131537
+*/
+
+SELECT
+    DATE_FORMAT(SALES_DATE, "%Y-%m-%d") AS SALES_DATE,
+    PRODUCT_ID,
+    USER_ID,
+    SALES_AMOUNT
+FROM
+    ONLINE_SALE
+WHERE
+    SALES_DATE BETWEEN "2022-03-01" AND "2022-03-31"
+
+UNION
+
+SELECT
+    DATE_FORMAT(SALES_DATE, "%Y-%m-%d") AS SALES_DATE,
+    PRODUCT_ID,
+    null AS USER_ID,
+    SALES_AMOUNT
+FROM
+    OFFLINE_SALE
+WHERE
+    SALES_DATE BETWEEN "2022-03-01" AND "2022-03-31"
+
+ORDER BY
+    SALES_DATE ASC,
+    PRODUCT_ID ASC,
+    USER_ID ASC
+;
