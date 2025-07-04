a67bd1b93c0e7a36aef437f8037b90129cb12188
Jang <hjjang501@gmail.com>
2025-07-04 14:12:12 +0900

    Add solution code 7/4/25

---
+++ programmers/293260.sql
@@ -0,0 +1,13 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/293260
+*/
+
+SELECT
+    COUNT(ID) AS FISH_COUNT,
+    MONTH(TIME) AS `MONTH`
+FROM
+    FISH_INFO
+GROUP BY
+    MONTH(TIME)
+ORDER BY
+    `MONTH`;
